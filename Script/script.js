9aede8219a24e58b0c7ab192248e8b724316dd10
mombeh nadine <nadinemombeh@gmail.com>
2025-03-26 15:34:28 +0100

    solve linters

--- Script/script.js
+++ Script/script.js
@@ -9,10 +9,10 @@ function clearDisplay () {
 }
 
 function calculate () {
-  try{
-    display.value = eval(display.value);
+  try {
+    display.value = eval(display.value)
   }
-  catch(error){
+  catch (error) {
     display.value = 'Error'
   }
 }

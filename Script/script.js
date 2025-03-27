6cde9beeaaeb7b4b2295467ccf47be0619b31dbc
mombeh nadine <nadinemombeh@gmail.com>
2025-03-27 15:45:29 +0100

    solve linters

--- Script/script.js
+++ Script/script.js
@@ -1,23 +1,22 @@
-const display = document.getElementById('display');
+const display = document.getElementById('display')
 
-function appendToDisplay(input) {
-  display.value += input;
+function appendToDisplay (input) {
+  display.value += input
 }
 
-function clearDisplay() {
-  display.value = '';
+function clearDisplay () {
+  display.value = ''
 }
 
-function calculate() {
+function calculate () {
   try {
     // Use math.js for safe calculations
-    display.value = math.evaluate(display.value);
+    display.value = math.evaluate(display.value)
   } catch (error) {
-    display.value = 'Error';
+    display.value = 'Error'
   }
 }
-
 // Attach functions to the window to prevent linter warnings
-window.appendToDisplay = appendToDisplay;
-window.clearDisplay = clearDisplay;
-window.calculate = calculate;
+window.appendToDisplay = appendToDisplay
+window.clearDisplay = clearDisplay
+window.calculate = calculate
